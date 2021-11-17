38cb6ebfdc84912ed134cfdae1d77e9b18f09437
AdriaPriego <adripg12@gmail.com>
2021-11-17 09:37:21 +0100

    Proba de pujada de fitxers 2

---
+++ app/src/main/java/com/example/proyectouia/ActivitatTransport.java
@@ -0,0 +1,14 @@
+package com.example.proyectouia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitatTransport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitat_transport);
+    }
+}
