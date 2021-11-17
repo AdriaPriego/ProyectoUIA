541867be906b51b2a9f2fc4a38fc96269f4164c7
AdriaPriego <adripg12@gmail.com>
2021-11-17 09:28:05 +0100

    Proba de pujada de fitxers 1

---
+++ app/src/main/java/com/example/proyectouia/ActivitatBusinesses.java
@@ -0,0 +1,14 @@
+package com.example.proyectouia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitatBusinesses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitat_businesses);
+    }
+}
